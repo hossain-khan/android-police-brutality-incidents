48986a20a0127a3c607d420f0a8e1ed809b5f9ce
Hossain Khan <amardeshbd@gmail.com>
2020-06-06 14:17:03 -0400

    [ADDED] Dagger injection support in Fragments.

--- android-app/app/src/main/java/com/github/policebrutality/ui/home/HomeFragment.kt
+++ android-app/app/src/main/java/com/github/policebrutality/ui/home/HomeFragment.kt
@@ -5,25 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.github.policebrutality.R
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
-class HomeFragment : Fragment() {
+class HomeFragment : DaggerFragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModel by viewModels<HomeViewModel> { viewModelFactory }
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        viewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- android-app/app/src/main/java/com/github/policebrutality/ui/notifications/NotificationsViewModel.kt
+++ android-app/app/src/main/java/com/github/policebrutality/ui/notifications/NotificationsViewModel.kt
@@ -3,8 +3,9 @@ package com.github.policebrutality.ui.notifications
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class NotificationsViewModel : ViewModel() {
+class NotificationsViewModel @Inject constructor() : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
