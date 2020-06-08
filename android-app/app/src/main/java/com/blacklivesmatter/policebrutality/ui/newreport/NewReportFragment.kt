341d256723a7315ca38f178270279b9b22c76144
Hossain Khan <amardeshbd@gmail.com>
2020-06-08 15:59:25 -0400

    [ADDED] Data binding for the report screen

--- android-app/app/src/main/java/com/blacklivesmatter/policebrutality/ui/newreport/NewReportFragment.kt
+++ android-app/app/src/main/java/com/blacklivesmatter/policebrutality/ui/newreport/NewReportFragment.kt
@@ -4,31 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.blacklivesmatter.policebrutality.R
+import com.blacklivesmatter.policebrutality.databinding.FragmentNewReportBinding
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
 class NewReportFragment : DaggerFragment() {
-
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private val viewModel by viewModels<NewReportViewModel> { viewModelFactory }
+    private lateinit var viewDataBinding: FragmentNewReportBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_new_report, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        viewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewDataBinding = FragmentNewReportBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = this@NewReportFragment
+            vm = viewModel
+        }
+
+        return viewDataBinding.root
     }
 }
