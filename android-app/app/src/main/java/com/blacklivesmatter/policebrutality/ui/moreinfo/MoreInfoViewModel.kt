160e65e734496f88bdd63d4f116cef7f8fa26a98
Hossain Khan <amardeshbd@gmail.com>
2020-06-08 23:37:22 -0400

    [ADDED] Social links for PB2020

--- android-app/app/src/main/java/com/blacklivesmatter/policebrutality/ui/moreinfo/MoreInfoViewModel.kt
+++ android-app/app/src/main/java/com/blacklivesmatter/policebrutality/ui/moreinfo/MoreInfoViewModel.kt
@@ -1,14 +1,29 @@
 package com.blacklivesmatter.policebrutality.ui.moreinfo
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.blacklivesmatter.policebrutality.R
+import com.blacklivesmatter.policebrutality.config.PB_LINK_FACEBOOK
+import com.blacklivesmatter.policebrutality.config.PB_LINK_INSTAGRAM
+import com.blacklivesmatter.policebrutality.config.PB_LINK_REDDIT
+import com.blacklivesmatter.policebrutality.config.PB_LINK_TWITTER
+import com.blacklivesmatter.policebrutality.config.PB_LINK_WEB
+import com.blacklivesmatter.policebrutality.ui.extensions.LiveEvent
+import timber.log.Timber
 import javax.inject.Inject
 
 class MoreInfoViewModel @Inject constructor() : ViewModel() {
+    private val _openExternalUrl = LiveEvent<String>()
+    val openExternalUrl: LiveData<String> = _openExternalUrl
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is more info Fragment"
+    fun onExternalResourceClicked(viewId: Int) {
+        when (viewId) {
+            R.id.pb2020_ext_link_reddit -> _openExternalUrl.value = PB_LINK_REDDIT
+            R.id.pb2020_ext_link_twitter -> _openExternalUrl.value = PB_LINK_TWITTER
+            R.id.pb2020_ext_link_instagram -> _openExternalUrl.value = PB_LINK_INSTAGRAM
+            R.id.pb2020_ext_link_facebook -> _openExternalUrl.value = PB_LINK_FACEBOOK
+            R.id.pb2020_ext_link_web -> _openExternalUrl.value = PB_LINK_WEB
+            else -> Timber.w("Unhandled new item?")
+        }
     }
-    val text: LiveData<String> = _text
 }
