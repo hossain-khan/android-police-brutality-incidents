af3a9c3e2cefbc7c8ad72927494d490049ec4bc6
Hossain Khan <amardeshbd@gmail.com>
2020-06-10 14:25:54 -0400

    [MINOR] Code format to make it more readable

--- android-app/app/src/main/java/com/blacklivesmatter/policebrutality/config/Constants.kt
+++ android-app/app/src/main/java/com/blacklivesmatter/policebrutality/config/Constants.kt
@@ -12,10 +12,12 @@ import org.threeten.bp.ZoneOffset
  * Time zone in Minneapolis, MN, USA (GMT-5)
  *
  * - https://en.wikipedia.org/wiki/George_Floyd
- * - https://en.wikipedia.org/wiki/8%E2%80%B246%E2%80%B3
+ * - https://en.wikipedia.org/wiki/8%E2%80%B246%E2%80%B3 (aka 8′46″)
  */
-val THE_846_DAY: OffsetDateTime = OffsetDateTime.of(/* year */ 2020, /* month */ 5, /* dayOfMonth */ 25,
-    /* hour */ 8, /* minute */ 19, /* second */ 0, /* nanoOfSecond */ 0, ZoneOffset.of("-5")
+val THE_846_DAY: OffsetDateTime = OffsetDateTime.of(
+    /* year */ 2020, /* month */ 5, /* dayOfMonth */ 25,
+    /* hour */ 8, /* minute */ 19, /* second */ 0,
+    /* nanoOfSecond */ 0, ZoneOffset.of("-5")
 )
 
 const val DATABASE_NAME = "incidents-db"
