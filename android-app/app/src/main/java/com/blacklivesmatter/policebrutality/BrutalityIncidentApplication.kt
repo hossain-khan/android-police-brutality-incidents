2f102cae20a3d33f525b99698b5645527c5e1a74
Hossain Khan <amardeshbd@gmail.com>
2020-06-15 19:13:19 -0400

    [REFACTOR] Moved timber logging classes to it's own package.
    Crashlytics error logging is now down through Timber RELEASE tree

--- android-app/app/src/main/java/com/blacklivesmatter/policebrutality/BrutalityIncidentApplication.kt
+++ android-app/app/src/main/java/com/blacklivesmatter/policebrutality/BrutalityIncidentApplication.kt
@@ -4,11 +4,12 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.blacklivesmatter.policebrutality.logging.TimberDebugTree
+import com.blacklivesmatter.policebrutality.logging.TimberReleaseTree
 import com.blacklivesmatter.policebrutality.ui.common.ThemeHelper
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import javax.inject.Inject
-import kotlin.math.abs
 
 @HiltAndroidApp
 class BrutalityIncidentApplication : Application(), Configuration.Provider {
@@ -22,9 +23,7 @@ class BrutalityIncidentApplication : Application(), Configuration.Provider {
         // Because it's #BlackLivesMatter
         ThemeHelper.applyTheme(AppCompatDelegate.MODE_NIGHT_YES)
 
-        if (BuildConfig.DEBUG) {
-            installLogging()
-        }
+        installLogging()
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
@@ -34,24 +33,10 @@ class BrutalityIncidentApplication : Application(), Configuration.Provider {
     }
 
     private fun installLogging() {
-        // Include logging in release builds so we can test from client.
-        // See https://github.com/JakeWharton/timber/blob/master/timber-sample/src/main/java/com/example/timber/ExampleApp.java
-        Timber.plant(object : Timber.DebugTree() {
-            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                val newMessage = "[${getThreadName()}] $message"
-                super.log(priority, tag, newMessage, t)
-            }
-
-            /**
-             * Returns name of current thread, with some well known thread names replaced to make easier to understand.
-             */
-            private fun getThreadName(): String {
-                var name = Thread.currentThread().name
-                if (name.length > 10) {
-                    name = name.substring(0, 10) + "." + abs(name.hashCode() % 100)
-                }
-                return name
-            }
-        })
+        if (BuildConfig.DEBUG) {
+            Timber.plant(TimberDebugTree())
+        } else {
+            Timber.plant(TimberReleaseTree())
+        }
     }
 }
